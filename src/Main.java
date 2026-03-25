1f1f99dfb2d442e0bae50a7e8fd22fd465bd5637
MehmetAliAkkoyun <mehmetaliakkoyun179@gmail.com>
2026-03-26 00:49:42 +0300

    Proje Tamamlandı (Revize)

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,14 @@ import model.Library;
 import ui.ConsoleUI;
 import util.DataInitializer;
 
-
 public class Main {
     public static void main(String[] args) {
 
         Library library = new Library(
-                "Üniversite Merkez Kütüphanesi",
-                "Üniversite Cad. No:1, Ankara");
-
+                "Universite Merkez Kutuphanesi",
+                "Universite Cad. No:1, Ankara");
 
         DataInitializer.populate(library);
-
 
         ConsoleUI ui = new ConsoleUI(library);
         ui.start();
--- src/util/DataInitializer.java
+++ src/util/DataInitializer.java
@@ -12,12 +12,10 @@ public class DataInitializer {
 
     public static void populate(Library library) {
 
-
         Librarian lib = new Librarian(
                 "Ayşe Kaya", "akaya@kutuphane.edu.tr", "05321234567",
                 "LIB001", "admin123", "Genel Koleksiyon");
         library.addLibrarian(lib);
-
 
         Author a1 = new Author("Orhan Pamuk",   "info@orhanpamuk.net", "05001111111", "Türk");
         Author a2 = new Author("Sabahattin Ali", "info@sabahattinaali.net", "05002222222", "Türk");
@@ -25,7 +23,6 @@ public class DataInitializer {
         library.addAuthor(a1);
         library.addAuthor(a2);
         library.addAuthor(a3);
-
 
         Book b1 = new Book("Orhan Pamuk", "Kar",
                 89.90, "12. Baskı", "978-975-08-0286-3", "İletişim Yayınları", 436);
@@ -38,7 +35,6 @@ public class DataInitializer {
         Book b5 = new Book("Ursula K. Le Guin", "Karanlığın Sol Eli",
                 95.00, "3. Baskı", "978-605-09-0284-1", "Metis Yayınları", 352);
 
-
         StudyBooks sb1 = new StudyBooks(
                 "Ahmet Kaya", "Java ile Nesne Yönelimli Programlama",
                 145.00, "2. Baskı", "978-605-123-001-1", "Seçkin Yayıncılık",
@@ -47,7 +43,6 @@ public class DataInitializer {
                 "Mehmet Demir", "Veri Yapıları ve Algoritmalar",
                 130.00, "1. Baskı", "978-605-123-002-2", "Seçkin Yayıncılık",
                 480, "Bilgisayar Bilimleri", "Üniversite", false);
-
 
         Journals j1 = new Journals(
                 "Çeşitli Yazarlar", "Nature",
@@ -58,22 +53,18 @@ public class DataInitializer {
                 75.00, "Vol.40", "978-001-000-002-2", "IEEE",
                 180, "Yazılım Mühendisliği", 40, "0740-7459");
 
-
         Magazines m1 = new Magazines(
                 "Editör Kurulu", "National Geographic Türkiye",
                 40.00, "Mart 2025", "978-002-000-001-1", "National Geographic",
                 120, "Aylık", 32025, "Coğrafya & Doğa");
 
-
         for (Book b : new Book[]{b1, b2, b3, b4, b5, sb1, sb2, j1, j2, m1}) {
             library.addBook(b);
         }
 
-
         a1.addBook(b1); a1.addBook(b2);
         a2.addBook(b3); a2.addBook(b4);
         a3.addBook(b5);
-
 
         Student s1 = new Student("Ali Veli", "Ankara, Çankaya", "05311112233",
                 "20210001", "Bilgisayar Mühendisliği", 3);
@@ -81,7 +72,6 @@ public class DataInitializer {
                 "20210002", "Türk Dili ve Edebiyatı", 2);
         Faculty f1 = new Faculty("Prof. Dr. Bülent Yılmaz", "Ankara, Üniversite Mah.", "05333334455",
                 "FAC001", "Prof. Dr.", "Bilgisayar Mühendisliği");
-
 
         s1.addBalance(500.0);
         s2.addBalance(300.0);
@@ -91,7 +81,7 @@ public class DataInitializer {
         library.addMember(s2);
         library.addMember(f1);
 
-        System.out.println("✓  Demo verileri yüklendi.");
+        System.out.println("OK: Demo verileri yüklendi.");
         System.out.printf("   → %d kitap, %d üye, %d yazar%n",
                 library.getTotalBooks(), library.getTotalMembers(), library.getAuthors().size());
     }
